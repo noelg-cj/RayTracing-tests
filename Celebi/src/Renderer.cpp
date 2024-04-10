c8bee7d4ce5b41e95a01a7ba89d67d0300c7f103
Noel George C J <noelgeorge2004@gmail.com>
2024-04-10 19:53:58 +0530

    [wip] Fragmentialization

--- Celebi/src/Renderer.cpp
+++ Celebi/src/Renderer.cpp
@@ -18,12 +18,25 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
+
 void Renderer::Render() {
-	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++) {
-		m_ImageData[i] = Walnut::Random::UInt();
-		m_ImageData[i] |= 0xff000000;
-		//m_ImageData[i] = 0xffffe0724a;
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) {
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
+			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+
+			//m_ImageData[i] = 0xffffe0724a;
+		}
 	}
+	
 
 	m_FinalImage->SetData(m_ImageData);
+}
+
+uint32_t Renderer::PerPixel(glm::vec2 coord)
+{
+	uint8_t r = (uint8_t)(coord.x * 255.0f);
+	uint8_t g = (uint8_t)(coord.y * 255.0f);
+
+	return 0xff000000 | (g << 8) | r;
 }
--- Celebi/src/Renderer.h
+++ Celebi/src/Renderer.h
@@ -3,6 +3,7 @@
 #include "Walnut/Image.h"
 
 #include <memory>
+#include <glm/glm.hpp>
 
 class Renderer
 {
@@ -12,8 +13,10 @@ public:
 	void OnResize(uint32_t width, uint32_t height);
 	void Render();
 
-	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage;  }
+	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; };
 
+private:
+	uint32_t PerPixel(glm::vec2 coord);
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
