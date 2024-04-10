5cd0a07695d7c159ffbc55f8e9ada2a23988ba96
Noel George C J <noelgeorge2004@gmail.com>
2024-04-10 15:55:13 +0530

    [feature] Viewport and GUI

--- Celebi/src/WalnutApp.cpp
+++ Celebi/src/WalnutApp.cpp
@@ -3,23 +3,54 @@
 
 #include "Walnut/Image.h"
 
+using namespace Walnut;
+
 class ExampleLayer : public Walnut::Layer
 {
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		if (ImGui::Button("Render")) {
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::Begin("Viewport");
+		
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image) {
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		}
+
+		ImGui::End();
 	}
+
+	void Render() {
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
+			m_ImageData[i] = 0xffffe0724a;
+		}
+
+		m_Image->SetData(m_ImageData);
+	}
+private:
+	std::shared_ptr<Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+	spec.Name = "Celebi Prototype";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
