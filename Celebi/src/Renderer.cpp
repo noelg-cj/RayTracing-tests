2d233798aab41642c41b0b6b4be3cd8bf686d91f
Noel George C J <noelgeorge2004@gmail.com>
2024-04-10 19:15:28 +0530

    [chore] Created Renderer Class

---
+++ Celebi/src/Renderer.cpp
@@ -0,0 +1,25 @@
+#include "Renderer.h"
+
+void Renderer::OnResize(uint32_t width, uint32_t height)
+{
+	if (m_FinalImage) {
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+			return;
+
+		m_FinalImage->Resize(width, height);
+	}
+	else {
+		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+	}
+
+	delete[] m_ImageData;
+	m_ImageData = new uint32_t[width * height];
+}
+
+void Renderer::Render() {
+	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++) {
+		m_ImageData[i] = 0xffffe0724a;
+	}
+
+	m_FinalImage->SetData(m_ImageData);
+}
--- Celebi/src/WalnutApp.cpp
+++ Celebi/src/WalnutApp.cpp
@@ -4,6 +4,8 @@
 #include "Walnut/Timer.h"
 #include "Walnut/Image.h"
 
+#include "Renderer.h"
+
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
@@ -24,34 +26,26 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if (m_Image) {
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		auto image = m_Renderer.GetFinalImage();
+		if (image) {
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() });
 		}
 
 		ImGui::End();
 		ImGui::PopStyleVar();
+
 	}
 
 	void Render() {
 		Timer timer;
 
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
-			m_ImageData[i] = 0xffffe0724a;
-		}
-
-		m_Image->SetData(m_ImageData);
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0;
