e6fe93355f2b19ee39754e7fa78c64d09ab368be
Noel George C J <noelgeorge2004@gmail.com>
2024-04-13 12:12:32 +0530

    [milestone] Camera motion added

--- Celebi/src/WalnutApp.cpp
+++ Celebi/src/WalnutApp.cpp
@@ -5,12 +5,19 @@
 #include "Walnut/Image.h"
 
 #include "Renderer.h"
+#include "Camera.h"
 
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
+	ExampleLayer()
+		: m_camera(45.0f, 0.1f, 100.0f) {}
+	virtual void OnUpdate(float ts) override
+	{
+		m_camera.OnUpdate(ts);
+	}
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
@@ -41,12 +48,14 @@ public:
 		Timer timer;
 
 		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Renderer.Render();
+		m_camera.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render(m_camera);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
 	Renderer m_Renderer;
+	Camera m_camera;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0;
