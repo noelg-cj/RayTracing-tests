4ada10ab1d7ee311c408768e5965ad2981fa0a9d
Noel George C J <noelgeorge2004@gmail.com>
2024-04-21 17:07:36 +0530

    [bugfix] syntax errors

--- Celebi/src/Scene.h
+++ Celebi/src/Scene.h
@@ -2,17 +2,17 @@
 #include <glm/glm.hpp>
 #include <vector>
 
-struct Material {
+struct Mat {
 	glm::vec3 Albedo{ 1.0f };
 	float Roughness = 1.0f;
-	float Metallic = 0.0f
+	float Metallic = 0.0f;
 };
 
 struct Sphere {
 	glm::vec3 Position{ 0.0f };
 	float Radius = 0.5f;
 
-	Material Mat;
+	Mat Mat;
 };
 
 struct Scene {
