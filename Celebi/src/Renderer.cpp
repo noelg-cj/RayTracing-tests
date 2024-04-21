0c49746a46c103cee340d5371e7c302e279d8239
Noel George C J <noelgeorge2004@gmail.com>
2024-04-22 00:23:40 +0530

    [FEAT] Multithreading

--- Celebi/src/Renderer.cpp
+++ Celebi/src/Renderer.cpp
@@ -2,6 +2,8 @@
 
 #include "Walnut/Random.h"
 
+#include <execution>
+
 
 namespace Utils {
 	static uint32_t ConvertToRGBA(const glm::vec4& color) {
@@ -32,6 +34,14 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 	delete[] m_AccumulationData;
 	m_AccumulationData = new glm::vec4[width * height];
+
+	m_ImageHorizontalIterator.resize(width);
+	m_ImageVerticalIterator.resize(height);
+
+	for (int i = 0; i < width; i++)
+		m_ImageHorizontalIterator[i] = i;
+	for (int i = 0; i < height; i++)
+		m_ImageVerticalIterator[i] = i;
 }
 
 
@@ -43,6 +53,25 @@ void Renderer::Render(const Scene& scene, const Camera& camera) {
 	if (m_FrameIndex == 1)
 		memset(m_AccumulationData, 0, m_FinalImage->GetWidth() * m_FinalImage->GetHeight() * sizeof(glm::vec4));
 
+#define MT 1
+#if MT
+	std::for_each(std::execution::par, m_ImageVerticalIterator.begin(), m_ImageVerticalIterator.end(),
+		[this](uint32_t y)
+		{
+			std::for_each(std::execution::par, m_ImageHorizontalIterator.begin(), m_ImageHorizontalIterator.end(),
+			[this, y](uint32_t x) {
+					glm::vec4 color = PerPixel(x, y);
+					m_AccumulationData[x + y * m_FinalImage->GetWidth()] += color;
+
+					glm::vec4 AccumulatedColor = m_AccumulationData[x + y * m_FinalImage->GetWidth()];
+					AccumulatedColor /= (float)m_FrameIndex;
+
+					AccumulatedColor = glm::clamp(AccumulatedColor, glm::vec4(0.0f), glm::vec4(1.0f));
+					m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(AccumulatedColor);
+				});
+		});
+
+#else
 	float aspectRatio = m_FinalImage->GetWidth() / (float)m_FinalImage->GetHeight();
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) 
 	{
@@ -58,7 +87,7 @@ void Renderer::Render(const Scene& scene, const Camera& camera) {
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(AccumulatedColor);
 		}
 	}
-	
+#endif
 
 	m_FinalImage->SetData(m_ImageData);
 
